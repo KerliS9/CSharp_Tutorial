a9536b4e75e0f071686c78329a7d22b14178a551
KerliS9 <kerlischroeder9@gmail.com>
2022-08-17 20:10:41 -0300

    feat - primeiros passos em C#

---
+++ console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// using System;
+
+namespace console;
+
+public class Program {
+  public static void Main(string[] args)
+  {
+    string person = "Kerli";
+    int age = 34;
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("My name is " + person);
+    Console.WriteLine("I am " + age + " years old.");
+    Console.WriteLine("I like my name " + person);
+    Console.WriteLine("And like being " + age);
+  }
+}
+
---
+++ dataType/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace data_type;
+
+public class DataType {
+  public static void Main() 
+  {
+    string phrase = "Kerli Schroeder";
+    char grade = 'A';
+    int age = 34;
+    // float, double, decimal - do menos para o mais preciso
+    double height = 1.70;
+    bool female = true;
+
+    Console.WriteLine("Name: " + phrase + "\n grade: " + grade + "\n age: " +  age + "\n height: " +  height + "\n female: " +  female);
+    Console.WriteLine("Kerli\"Schroeder"); // imprimir as aspas
+    Console.WriteLine(phrase.ToUpper()); // ACESSANDO MÉTODOS
+    Console.WriteLine(phrase.Contains("der")); // verifica se contêm esta informação, retorna um boolean.
+    Console.WriteLine(phrase[0]); // retorna o index
+    Console.WriteLine(phrase.IndexOf("li")); // mostra o index da palavra pesquisada
+    Console.WriteLine(phrase.Substring(6, 5)); // identifica o início da extração e quantos dados serão extraídos
+  }
+}

753161cee2c9633b577fef85adfd0e507821b08b
KerliS9 <kerlischroeder9@gmail.com>
2022-08-19 16:10:14 -0300

    feat - file for debugging

---
+++ debugging/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace DebuggingCSharp
+{
+  class Program
+  {
+    public static void Main()
+    {
+      var numbers = new List<int>{ 1, 2, 3, 4, 5, 6 };
+      var smallest = GetSmallests(numbers, 3); // retorne os 3 menores números desta lista
+
+      foreach (var number in smallest)
+        Console.WriteLine(number);
+    }
+    public static List<int> GetSmallests(List<int> list, int count)
+    {
+      var smallest = new List<int>();
+      while (smallest.Count < count)
+      {
+        var min = GetSmallest(list);
+        // System.Console.WriteLine(min);
+        smallest.Add(min); // adiciona na nova lista smallest
+        list.Remove(min); // remove da lista antiga
+      }
+      return smallest;
+    }
+
+    public static int GetSmallest(List<int> list)
+    {
+      var min = list[0];
+      for (var i = 1; i < list.Count; i++)
+      {
+        if(list[i] < min) min = list[i];
+      }
+      return min;
+    }
+  }
+}
+
+
---
+++ debugging/fileLaunch.js
@@ -0,0 +1,35 @@
+/* {
+  // Use IntelliSense to learn about possible attributes.
+  // Hover to view descriptions of existing attributes.
+  // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": ".NET Core Launch (console)",
+      "type": "coreclr",
+      "request": "launch",
+      "preLaunchTask": "build",
+      "program": "${workspaceFolder}/bin/Debug/net6.0/debugging.dll",
+      "args": [],
+      "cwd": "${workspaceFolder}",
+      "console": "internalConsole",
+      "stopAtEntry": false
+    },
+    {
+      "name": ".NET Core Attach",
+      "type": "coreclr",
+      "request": "attach"
+    }
+  ]
+} */
+
+// este arquivo terá o formato .json | será criada uma pasta .vscode
+
+// para criá-lo acesse a pasta do projeto
+// marque a linha de início do debugging
+// clique em Create a launch.json file (logo abaixo do Run and Debug)
+// escolha a stack .Net 5+ and ...
+// Debugger habilitado para processar
+// na linha 12, acima, irá mostrar em qual projeto está conectado 
+
+// https://www.youtube.com/watch?v=-Bl6sUDp-CQ
